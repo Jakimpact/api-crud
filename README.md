297c764f713587c73173ebd9bad408de560f2e90
Quentin <jakimpact@gmail.com>
2025-01-22 16:23:05 +0100

    Authentification fonctionnelle

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # api-crud
+
+## Description du projet
+
+Ce projet est une API CRUD avec authentification OAuth2, permettant de gérer des produits. Les opérations disponibles incluent :
+- Lister les produits
+- Consulter un produit spécifique
+- Ajouter un produit
+- Modifier un produit
+- Supprimer un produit
+
+L'API utilise FastAPI pour la gestion des routes et SQLModel pour l'interaction avec la base de données.
+
+## Installation et utilisation
+
+### Prérequis
+
+Assurez-vous d'avoir Python 3.8+ installé sur votre machine.
+
+### Installation
+
+1. Clonez le dépôt :
+    ```sh
+    git clone <git@github.com:Jakimpact/api-crud.git>
+    cd <api-crud>
+    ```
+
+2. Créez un environnement virtuel et activez-le :
+    ```sh
+    python -m venv venv
+    source venv/bin/activate  # Sur Windows, utilisez `venv\Scripts\activate`
+    ```
+
+3. Installez les dépendances :
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+### Configuration
+
+Créez un fichier `.env` à la racine du projet et définissez les variables suivantes :
+
+USER="your_username"
+PASSWORD="your_password"
+SERVER_NAME="your_sql_server"
+BDD_NAME="your_bbd_name"
+PORT=1433
+SECRET_KEY="your_secret_key"
+ALGORITHM="your_algorithm"
+
+### Lancer le projet
+
+Pour démarrer le serveur FastAPI, exécutez la commande suivante :
+```sh
+fastapi endpoints.py
+```
+
+## Routes de l'API
+
+- **GET /** : Retourne un message de bienvenue.
+- **POST /token** : Authentifie un utilisateur et retourne un token d'accès.
+- **GET /products** : Liste tous les produits *(nécessite une authentification)*.
+- **GET /products/{product_id}** : Retourne les détails d'un produit spécifique *(nécessite une authentification)*.
+- **POST /products** : Ajoute un nouveau produit *(nécessite une authentification)*.
+- **PUT /products/{product_id}** : Modifie un produit existant *(nécessite une authentification)*.
+- **DELETE /products/{product_id}** : Supprime un produit *(nécessite une authentification)*.
+
+---
+
+## Utilisation de SQLModel
+
+Le projet utilise **SQLModel** pour interagir avec la base de données.  
+Les modèles sont définis dans le fichier `models.py`.  
+La connexion à la base de données est gérée dans le fichier `connect_db.py`.
+
+---
+
+## Authentification
+
+L'authentification est gérée via **OAuth2 avec mot de passe**.  
+Les fonctions d'authentification sont définies dans le fichier `generate_auth.py`.  
+Les utilisateurs sont stockés dans une base de données fictive `users_db`.
+
+Pour obtenir un token d'accès, envoyez une requête **POST** à `/token` avec les informations d'identification de l'utilisateur.  
+Utilisez ce token pour accéder aux routes protégées de l'API.
--- endpoints.py
+++ endpoints.py
@@ -1,9 +1,16 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlmodel import select, Session
-from typing import List
+from datetime import timedelta
+from typing import Annotated, List
 
-from models import Product
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import select, Session
+
 from connect_db import get_session, ENGINE
+from generate_auth import authenticate_user, create_access_token, get_current_active_user, Token, User, users_db
+from models import Product
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI(
     title="API CRUD Adventuresworks",
@@ -23,8 +30,28 @@ async def home():
     return {"Hello": "HELLO THERE"}
 
 
+@app.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    """
+    Login un utilisateur et récupère un token d'authentification.
+    """
+
+    user = authenticate_user(users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
 @app.get("/products", response_model=List[Product])
-async def get_products(session: Session=Depends(get_session)):
+async def get_products(current_user: Annotated[User, Depends(get_current_active_user)], session: Session=Depends(get_session)):
     """
     Liste l'ensemble des produits de la base de données.
     """
@@ -35,7 +62,7 @@ async def get_products(session: Session=Depends(get_session)):
     
 
 @app.get("/products/{product_id}", response_model=Product)
-async def get_product(product_id: int, session: Session=Depends(get_session)):
+async def get_product(current_user: Annotated[User, Depends(get_current_active_user)], product_id: int, session: Session=Depends(get_session)):
     """
     Récupère un produit spécifique de la base de données.
     Params :
@@ -49,7 +76,7 @@ async def get_product(product_id: int, session: Session=Depends(get_session)):
 
 
 @app.post("/products", response_model=Product)
-async def add_product(product: Product, session: Session=Depends(get_session)):
+async def add_product(current_user: Annotated[User, Depends(get_current_active_user)], product: Product, session: Session=Depends(get_session)):
     """
     Crée un nouveau produit dans la base de données.
     Params :
@@ -64,7 +91,7 @@ async def add_product(product: Product, session: Session=Depends(get_session)):
 
 
 @app.put("/products/{product_id}",response_model=Product)
-async def update_product(product: Product, product_id: int, session: Session=Depends(get_session)):
+async def update_product(current_user: Annotated[User, Depends(get_current_active_user)], product: Product, product_id: int, session: Session=Depends(get_session)):
     """
     Modifie un produit existant dans la base de données.
     Params :
@@ -85,7 +112,7 @@ async def update_product(product: Product, product_id: int, session: Session=Dep
 
 
 @app.delete("/products/{product_id}", response_model=Product)
-async def delete_product(product_id: int, session: Session=Depends(get_session)):
+async def delete_product(current_user: Annotated[User, Depends(get_current_active_user)], product_id: int, session: Session=Depends(get_session)):
     """
     Supprime un produit de la base de données.
     Params :
